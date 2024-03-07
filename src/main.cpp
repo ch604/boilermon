ec640e81b9fac9a42007459c1ecb77953feb9c5f
Andrej Walilko <awalilko@liquidweb.com>
2024-03-07 12:20:07 -0500

    inits

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
-// Temp Sensor
+// INCLUDES //
+// temp sensors
 #include "OneWire.h"
 #include "DallasTemperature.h"
-// I2C screen
+// screen
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-OneWire oneWire(13); // GPIO13
+// DEFINITIONS //
+// temp sensors
+OneWire oneWire(13); // GPIO13 for all temp sensor data pins
 DallasTemperature sensors(&oneWire);
+int total_tempsens
 
+// screen
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16x2 display
+
+// STARTUP //
 void setup()
 {
+	Serial.begin(115200);
+
+	// temp sensors
 	sensors.begin();
+	total_tempsens = sensors.getDeviceCount();
+
+	// screen
+	lcd.init();
+	lcd.backlight();
 }
 
+// MAIN LOOP //
 void loop()
 {
+	sensors.requestTemperatures();
 }
